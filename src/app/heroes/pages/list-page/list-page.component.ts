1dfb09e8baa57057f835d39b276f863c7e3953d0
Pamela Gutierrez <p.gutierrez@canalcero.com>
2025-04-20 10:53:01 -0400

    creacion de variables de entorno y servicios

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-list-page',
@@ -6,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './list-page.component.html',
   styles: ``
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit{
+
+  public heroes: Hero[] = [];
+
+  constructor(private heroesService: HeroesService){}
+
+  ngOnInit(): void {
+   this.heroesService.getHero()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+
+
+
+
 
 }
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,3 @@
+export const environments = {
+  baseUrl: 'https://prod.com/api'
+}
