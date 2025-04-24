d84e12fd7809c6a250b9acdd44447b30a5630980
Pamela Gutierrez <p.gutierrez@canalcero.com>
2025-04-23 22:11:55 -0400

    pagina de buscar, se conecto con api y se creado el formulario del heroe para crear o eliminar

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -8,4 +12,28 @@ import { Component } from '@angular/core';
 })
 export class SearchPageComponent {
 
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectHero ?: Hero;
+
+  constructor(private heroService: HeroesService){}
+
+  searchHero(){
+    const value: string = this.searchInput.value || '';
+
+    this.heroService.getSuggestions(value)
+      .subscribe( heroes => this.heroes = heroes);
+  }
+
+  onSelectedOption( event: MatAutocompleteSelectedEvent ):void{
+    if(!event.option.value){
+      this.selectHero = undefined;
+      return;
+    }
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue( hero.superhero );
+
+    this.selectHero = hero;
+  }
 }
